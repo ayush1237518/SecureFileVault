2f7f216633ba700023c037a5390d5568cdd518e1
ayush1237518 <ayushs9378@gmail.com>
2026-07-14 21:19:27 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // Replace SecureFileVault with your repository name
-  base: '/SecureFileVault/',
-
   server: {
     host: '0.0.0.0',
     port: 5173,
